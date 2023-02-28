ace07799d8dba818ecba54f030dfc8c78fc0d54f
Sravan T P <sravantp03@gmail.com>
2023-02-28 16:25:52 +0530

    deployed Whitelist.sol to goerli testnet

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,6 +1,15 @@
 const { ethers } = require("hardhat")
 
-async function main() {}
+// contract address on goerli : 0xcEbEF944e2fbe18de222A22157E86AeB21507940
+
+async function main() {
+    const WhitelistFactory = await ethers.getContractFactory("Whitelist")
+
+    // Deploying Contract
+    const contract = await WhitelistFactory.deploy("5")
+    await contract.deployed()
+    console.log(`Contract Deployed at address : ${contract.address}`)
+}
 
 main()
     .then(() => process.exit(0))
